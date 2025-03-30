e81a633c921c04e9f93ee53f03c6a7c4f64953a6
qamar serhan <serhanqamar@gmail.com>
2025-03-30 05:40:08 +0300

    xD

--- src/config/initdb.js
+++ src/config/initdb.js
@@ -1,34 +1,39 @@
 //initialize db
-require('dotenv').config(); // Load environment variables from .env
-const mysql = require('mysql2/promise'); //create a connection to the MySQL database.
+require("dotenv").config(); // Load environment variables from .env
+const mysql = require("mysql2/promise"); //create a connection to the MySQL database.
 
 async function initDatabase() {
-    try {
-        console.log("DB Config:", process.env.DB_USER, process.env.DB_PASSWORD ? "Has Password" : "No Password"); // Debug
+  try {
+    console.log(
+      "DB Config:",
+      process.env.DB_USER,
+      process.env.DB_PASSWORD ? "Has Password" : "No Password"
+    ); // Debug
 
-        // Establish Connection
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST || 'localhost',
-            user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || '',  // Ensure it reads the password
-            database: process.env.DB_NAME || 'shestyle_db',
-            port: process.env.DB_PORT || 3306
-        });
+    // Establish Connection
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASSWORD || "", // Ensure it reads the password
+      database: process.env.DB_NAME || "water_db",
+      port: process.env.DB_PORT || 3306,
+    });
 
-        console.log("Connected to MySQL!");
+    console.log("Connected to MySQL!");
 
-        // await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
-        const [rows] = await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
-        console.log("Database created or already exists.");
-        
-        await connection.end();
-    } catch (error) {
-        console.error("Error during database initialization:", error);
-    }
+    // await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
+    const [rows] = await connection.query(
+      `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`
+    );
+    console.log("Database created or already exists.");
+
+    await connection.end();
+  } catch (error) {
+    console.error("Error during database initialization:", error);
+  }
 }
 
 initDatabase();
-
 
 /* //delete and init database 
 
