231d0c1b8a43bb780eb7b7756fae0ae227699438
qamar serhan <serhanqamar@gmail.com>
2025-04-15 01:05:17 +0300

    deploy db on railway

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,19 +1,31 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,  // Aiven MySQL Host
-  dialect: 'mysql',  // MySQL dialect for Sequelize
-  logging: false,  // Set to true for debugging
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,  // Aiven requires SSL connections
-    },
-  },
-} 
-  // {
-  //   host: process.env.DB_HOST,
-  //   dialect: 'mysql',
-  //
-);  
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+    logging: false,
+  }
+);
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,  // Aiven MySQL Host
+//   dialect: 'mysql',  // MySQL dialect for Sequelize
+//   logging: false,  // Set to true for debugging
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: false,  // Aiven requires SSL connections
+//     },
+//   },
+// }
+// {
+//   host: process.env.DB_HOST,
+//   dialect: 'mysql',
+//
+// );
 module.exports = sequelize;
